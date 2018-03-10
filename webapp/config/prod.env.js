93ceef8ae472f6b05990b1312ebfa4c7e0fb42e0
Benjamin Lacroix <b.lacroix@betclicgroup.com>
2018-03-10 16:58:10 +0100

    Configure Firebase hosting

--- webapp/config/prod.env.js
+++ webapp/config/prod.env.js
@@ -1,4 +1,12 @@
-'use strict'
+'use strict';
 module.exports = {
-  NODE_ENV: '"production"'
-}
+  NODE_ENV: '"production"',
+  FIREBASE_CONF: JSON.stringify({
+    apiKey: "AIzaSyA9DoFlwXCj_edYgjMTI1JYan3r6hMLUuY",
+    authDomain: "android-makers-stand.firebaseapp.com",
+    databaseURL: "https://android-makers-stand.firebaseio.com",
+    projectId: "android-makers-stand",
+    storageBucket: "android-makers-stand.appspot.com",
+    messagingSenderId: "269643408137"
+  }),
+};
