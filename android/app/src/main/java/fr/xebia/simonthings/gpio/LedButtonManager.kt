d1802eaaa823a6d81bcf4562b291e6848ea81300
Michael Ohayon <mohayon75@gmail.com>
2018-03-19 00:58:15 +0100

    Do not merge this. ThrottleFirst inputs

--- android/app/src/main/java/fr/xebia/simonthings/gpio/LedButtonManager.kt
+++ android/app/src/main/java/fr/xebia/simonthings/gpio/LedButtonManager.kt
@@ -4,13 +4,21 @@ import com.google.android.things.contrib.driver.button.Button
 import com.google.android.things.pio.Gpio
 import com.google.android.things.pio.PeripheralManager
 import fr.xebia.simonthings.engine.GameInputButton
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import io.reactivex.subjects.PublishSubject
+import java.util.concurrent.TimeUnit
 
 class LedButtonManager {
+
+    private val buttonPressSubject = PublishSubject.create<GameInputButton>()
+    private val disposables = CompositeDisposable()
 
     fun init(peripheralManager: PeripheralManager, buttonPressedCallback: (GameInputButton) -> Unit) {
         CONTROLS.forEach {
             val button = Button(it.buttonGpioName, Button.LogicState.PRESSED_WHEN_LOW)
-            button.setOnButtonEventListener { _, _ -> buttonPressedCallback(it.gameInputButton) }
+            button.setOnButtonEventListener { _, _ -> buttonPressSubject.onNext(it.gameInputButton) }
             it.button = button
 
             val led = peripheralManager.openGpio(it.ledGpioName)
@@ -20,9 +28,20 @@ class LedButtonManager {
 
             it.ledGpio = led
         }
+
+        disposables.add(
+                buttonPressSubject
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .throttleFirst(300, TimeUnit.MILLISECONDS)
+                        .subscribe {
+                            buttonPressedCallback(it)
+                        })
     }
 
     fun release() {
+        disposables.clear()
+
         CONTROLS.forEach {
             it.ledGpio?.close()
             it.button?.close()
